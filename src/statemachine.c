e7b5ec05ab325d4790bc9c3cb3345839aac04acd
Matthew Finkel <Matthew.Finkel@gmail.com>
2015-11-30 02:48:40 +0000

    Implement the base state transition functionality

--- src/statemachine.c
+++ src/statemachine.c
@@ -11,35 +11,102 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <string.h>
+
+/* libnok include */
 #include <statemachine.h>
 
 
 int
 libnok_state_fill_sequence(libnok_state_base_t *ctx_base)
 {
-  return -1;
+  int i;
+  if (ctx_base == NULL)
+    return -1;
+  if (ctx_base->sequence == NULL)
+    return -1;
+  if (ctx_base->len < 1)
+    return -1;
+
+  for (i = 0; i < ctx_base->len; ++i) {
+    ctx_base->sequence[i] = i;
+  }
+  return 0;
 }
 
 libnok_state_base_t *
 libnok_state_init_base(int len, int *seq)
 {
-  return NULL;
+  libnok_state_base_t *ctx_base = NULL;
+  if (seq == NULL)
+    return NULL;
+  if (len < 1)
+    return NULL;
+
+  ctx_base = (libnok_state_base_t *) malloc(sizeof(*ctx_base));
+  if (ctx_base == NULL)
+    return NULL;
+  ctx_base->sequence = (int *) malloc(sizeof(*(ctx_base->sequence))*len);
+  if (ctx_base->sequence == NULL) {
+    free(ctx_base);
+    return NULL;
+  }
+  ctx_base->len = len;
+  memcpy(ctx_base->sequence, seq, sizeof(*(ctx_base->sequence))*len);
+  return ctx_base;
 }
 
 libnok_state_t *
 libnok_state_init(int (*statetrans)(int *prev, int *curr, int *next),
                   libnok_state_base_t *ctx_base)
 {
-  return NULL;
+  libnok_state_t *ctx = NULL;
+
+  if (statetrans == NULL)
+    return NULL;
+  if (ctx_base == NULL) {
+    ctx_base = (libnok_state_base_t *) malloc(sizeof(*ctx_base));
+    if (ctx_base == NULL)
+      return NULL;
+  }
+
+  ctx = (libnok_state_t *) malloc(sizeof(*ctx));
+  if (ctx == NULL)
+    return NULL;
+
+  ctx->base = ctx_base;
+  ctx->prev_state = -1;
+  ctx->curr_state = -1;
+  ctx->next_state = -1;
+  ctx->statetrans = statetrans;
+
+  return ctx;
 }
 
 int
 libnok_state_get_next_state(libnok_state_t *ctx)
 {
-  return -1;
+  int prev = -1, curr = -1, next = -1;
+
+  if (ctx == NULL)
+    return -1;
+  if (ctx->statetrans == NULL)
+    return -1;
+
+  prev = ctx->prev_state;
+  curr = ctx->curr_state;
+  next = ctx->next_state;
+  if (ctx->statetrans(&prev, &curr, &next) == -1)
+    return -1;
+  ctx->prev_state = prev;
+  ctx->curr_state = curr;
+  ctx->next_state = next;
+
+  return 0;
 }
